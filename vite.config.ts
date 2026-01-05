6c2b041ed133848e992b5f17624e8aafb30780c1
magdykhelfa <magdy.khelfa.eg@gmail.com>
2026-01-05 13:16:01 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/gam3ia/",
+  base: "./",
   server: {
     host: "::",
     port: 8080,
