e3b1e7a3af0c0756683f2a9afccaa8a1f869a06e
magdykhelfa <magdy.khelfa.eg@gmail.com>
2026-01-05 13:51:06 +0200

    حذف الفولدر

--- auth/RequireAuth.tsx
+++
@@ -1,6 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export default function RequireAuth({ children }: { children: JSX.Element }) {
-  const isAuth = localStorage.getItem("auth") === "true";
-  return isAuth ? children : <Navigate to="/login" replace />;
-}
