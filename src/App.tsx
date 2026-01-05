446fbadbc5700e443d10601177a4cf7ab34b7e7b
magdykhelfa <magdy.khelfa.eg@gmail.com>
2026-01-05 13:54:02 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter, Routes, Route } from "react-router-dom";
 
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
-import Login from "./auth/Login";
-import ProtectedRoute from "./auth/ProtectedRoute";
+import Index from "@/pages/Index";
+import NotFound from "@/pages/NotFound";
+import Login from "@/auth/Login";
+import ProtectedRoute from "@/auth/ProtectedRoute";
 
 const queryClient = new QueryClient();
 
@@ -18,10 +18,8 @@ const App = () => (
       <Sonner />
       <HashRouter>
         <Routes>
-          {/* Login */}
           <Route path="/login" element={<Login />} />
 
-          {/* Protected Home */}
           <Route
             path="/"
             element={
@@ -31,7 +29,6 @@ const App = () => (
             }
           />
 
-          {/* Not Found */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </HashRouter>
