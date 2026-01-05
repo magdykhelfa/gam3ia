72a80716a2a5d8474ac647416a297c9d8b9188e3
magdykhelfa <magdy.khelfa.eg@gmail.com>
2026-01-05 13:58:33 +0200

    Update ProtectedRoute.tsx

--- src/auth/ProtectedRoute.tsx
+++ src/auth/ProtectedRoute.tsx
@@ -1,6 +1,11 @@
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }: { children: JSX.Element }) {
-  const isAuth = localStorage.getItem("auth") === "true";
-  return isAuth ? children : <Navigate to="/login" replace />;
+  const isAuth = localStorage.getItem("auth");
+
+  if (!isAuth) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
 }
